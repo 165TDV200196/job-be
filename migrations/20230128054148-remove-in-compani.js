6d254d89540a969620ac282b2c7d6b290635aa96
Văn kiên <chjkien9x@gmail.com>
2023-01-28 17:57:11 +0700

    up

---
+++ migrations/20230128054148-remove-in-compani.js
@@ -0,0 +1,10 @@
+// 'use strict';
+
+// module.exports = {
+//       down: async (queryInterface, Sequelize) => {
+//         return Promise.all([
+//           queryInterface.removeColumn('companies', 'nation'),
+//           queryInterface.removeColumn('companies', 'nation'),
+//         ]);
+//       },
+// };

72948c97598d73d06e67723ddf02dcbeb7025665
John <56056004+vankiendhv@users.noreply.github.com>
2023-05-04 17:06:47 +0700

    up

--- migrations/20220623075928-addSchedule.js
+++ migrations/20220623075928-addSchedule.js
@@ -1,16 +1,21 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    queryInterface.addColumn('workapplies', 'sechedule', {
-      type: Sequelize.STRING,
-      allowNull: true,
-    });
-  },
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('NotificationCompanies');
+        await queryInterface.dropTable('NotificationUsers');
+        await queryInterface.dropTable('TagCandidates');
+        await queryInterface.dropTable('Candidates');
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    return Promise.all([
-      queryInterface.removeColumn('workapplies', 'sechedule'),
-    ]);
-  },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('NotificationCompanies', {
+        });
+        await queryInterface.createTable('NotificationUsers', {
+        });
+        await queryInterface.createTable('TagCandidates', {
+        });
+        await queryInterface.createTable('Candidates', {
+        });
+    },
 };
